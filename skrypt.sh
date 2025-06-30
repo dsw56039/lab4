ffec7a873c2b01240a7d98dae06ee739c166cf46
Maciej <dsw56039@student.dsw.edu.pl>
2025-06-30 22:48:30 +0200

    dodano flagi i tak dalej

--- skrypt.sh
+++ skrypt.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
 
 case $1 in
-  --date)
+  --date|-d)
+    # Wyświetlenie dzisiejszej daty
     date
     ;;
-  --logs)
+
+  --logs|-l)
+    # Tworzenie plików logx.txt
     count=${2:-100}
     for i in $(seq 1 $count); do
       filename="log${i}.txt"
-      echo "Nazwa pliku: $filename" > $filename
-      echo "Utworzony przez: skrypt.sh" >> $filename
-      echo "Data: $(date)" >> $filename
+      echo "Plik: $filename" > "$filename"
+      echo "Utworzony przez: skrypt.sh" >> "$filename"
+      echo "Data: $(date)" >> "$filename"
     done
     ;;
-  --help)
-    echo "Dostępne opcje:"
-    echo "--date          - wyświetla datę"
-    echo "--logs [n]      - tworzy n plików log (domyślnie 100)"
-    echo "--help          - wyświetla tę pomoc"
+
+  --error|-e)
+    # Tworzenie plików errorx/errorX.txt
+    count=${2:-100}
+    mkdir -p errorx
+    for i in $(seq 1 $count); do
+      filename="errorx/error${i}.txt"
+      echo "Plik: $filename" > "$filename"
+      echo "Utworzony przez: skrypt.sh" >> "$filename"
+      echo "Data: $(date)" >> "$filename"
+    done
     ;;
+
+  --init)
+    # Klonowanie repozytorium i dodanie ścieżki do PATH
+    repo_url="https://github.com/dsw56039/lab4.git"  
+    dest_dir="$(pwd)/repo_clone"
+    git clone "$repo_url" "$dest_dir"
+    export PATH="$dest_dir:$PATH"
+    echo "Repozytorium sklonowane do $dest_dir i dodane do PATH"
+    ;;
+
+  --help|-h)
+    # Wyświetlenie dostępnych opcji
+    echo "Dostępne opcje:"
+    echo "--date / -d         - wyświetla dzisiejszą datę"
+    echo "--logs / -l [n]     - tworzy n plików log (domyślnie 100)"
+    echo "--error / -e [n]    - tworzy n plików błędów w katalogu errorx/ (domyślnie 100)"
+    echo "--init              - klonuje repozytorium i dodaje do PATH"
+    echo "--help / -h         - wyświetla tę pomoc"
+    ;;
+
   *)
-    echo "Nieznana opcja. Użyj --help"
+    echo "Nieznana opcja: $1"
+    echo "Użyj --help lub -h aby zobaczyć dostępne opcje."
     ;;
 esac
+
